b22852604006c896d39507ea961afae25ef70325
Nick Firme <nickfirme@live.com>
2021-12-11 19:03:16 -0800

    Replace Bootstrap with MUI

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -1,7 +1,72 @@
-// 1. check if token exists in local storage
-// 2. if token exists in local storage, retrieve it
-// 3. else, get URL parameters, store token in local storage, retrieve it
+import axios from 'axios';
 
+export const getURLParams = () => {
+  const URLparams = new URLSearchParams(window.location.search);
+  return Object.fromEntries(URLparams);
+};
 
+// TOKENS ******************************************************************************************
+const EXPIRATION_TIME = 3600 * 1000; // 3600 seconds * 1000 = 1 hour in milliseconds
 
-export const token = '';
+const setTokenTimestamp = () => window.localStorage.setItem('spotify_token_timestamp', Date.now());
+const setLocalAccessToken = token => {
+  setTokenTimestamp();
+  window.localStorage.setItem('spotify_access_token', token);
+};
+const setLocalRefreshToken = token => window.localStorage.setItem('spotify_refresh_token', token);
+const getTokenTimestamp = () => window.localStorage.getItem('spotify_token_timestamp');
+const getLocalAccessToken = () => window.localStorage.getItem('spotify_access_token');
+const getLocalRefreshToken = () => window.localStorage.getItem('spotify_refresh_token');
+
+// Refresh the token
+const refreshAccessToken = async () => {
+  try {
+    const localRefreshToken = getLocalRefreshToken();
+    if(localRefreshToken !== null || localRefreshToken !== 'undefined') {
+      const { data } = await axios.get(`/refresh_token?refresh_token=${localRefreshToken}`);
+      const { access_token } = data;
+      setLocalAccessToken(access_token);
+      window.location.reload();
+    }
+    return;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+// Get access token off of query params (called on application init)
+export const getAccessToken = () => {
+  const { error, access_token, refresh_token } = getURLParams();
+
+  if (error) {
+    console.error(error);
+    refreshAccessToken();
+  }
+
+  // If token has expired
+  if (Date.now() - getTokenTimestamp() > EXPIRATION_TIME) {
+    console.warn('Access token has expired, refreshing...');
+    refreshAccessToken();
+  }
+
+  const localAccessToken = getLocalAccessToken();
+
+  // If there is no ACCESS token in local storage, set it and return `access_token` from params
+  if ((!localAccessToken || localAccessToken === 'undefined') && access_token) {
+    setLocalAccessToken(access_token);
+    setLocalRefreshToken(refresh_token);
+    return access_token;
+  }
+
+  console.log(localAccessToken);
+  return localAccessToken;
+};
+
+export const token = getAccessToken();
+
+export const logout = () => {
+  window.localStorage.removeItem('spotify_token_timestamp');
+  window.localStorage.removeItem('spotify_access_token');
+  window.localStorage.removeItem('spotify_refresh_token');
+  window.location.replace('http://localhost:3000/');
+};
---
+++ client/src/spotify/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { token } from '../auth';
+
+const headers = {
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json',
+  };
+
+/*** API Calls ***/
+
+export const getPlaylists = () => axios.get('https://api.spotify.com/v1/me/playlists', { headers });
+

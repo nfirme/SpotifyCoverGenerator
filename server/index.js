68d91edb9f256cace67ec0abd913e479d8f6e1f2
Nick Firme <nickfirme@live.com>
2021-12-12 23:00:28 -0800

    Refactor to use .env and remove querystring

--- server/index.js
+++ server/index.js
@@ -1,60 +1,92 @@
+require('dotenv').config();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:8888/callback'
+const FRONTEND_URI = process.env.FRONTEND_URI || 'http://localhost:3000';
+const PORT = process.env.PORT || 8888;
+
 const express = require('express');
 const request = require('request');
 const cors = require('cors');
-const querystring = require('querystring');
-
-const PORT = 8888;
-
-const REDIRECT_URI = 'http://localhost:8888/callback';
-const FRONTEND_URI = 'http://localhost:3000/';
-
-const CLIENT_ID = '0ec9eb19c6b645a8993b5054120379f8';
-const CLIENT_SECRET = '6ab0a9ace0ac4f7aaebf95c16a375ea1';
 
 const app = express();
 app.use(cors());
 
+////// LOGIN ROUTE
 app.get('/login', (req, res) => {
   const scope = 'playlist-read-private';
-  const loginOptions = {
+  const queryParams = new URLSearchParams({
     client_id: CLIENT_ID,
     response_type: 'code',
     redirect_uri: REDIRECT_URI,
     scope: scope,
-  };
-
-  res.redirect(
-    `https://accounts.spotify.com/authorize?${querystring.stringify(
-      loginOptions
-    )}`
-  );
+  }).toString();
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
+////// CALLBACK ROUTE
 app.get('/callback', (req, res) => {
-  const code = req.query.code;
+  const code = req.query.code || null;
+  const buffer = new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+    'base64'
+  );
   const authOptions = {
     url: 'https://accounts.spotify.com/api/token',
     form: {
       code: code,
       redirect_uri: REDIRECT_URI,
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
       grant_type: 'authorization_code',
     },
-    json: true
+    headers: {
+      Authorization: `Basic ${buffer}`,
+    },
+    json: true,
   };
-  request.post(authOptions, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      const tokens = {
+
+  const callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const params = new URLSearchParams({
         access_token: body.access_token,
         refresh_token: body.refresh_token,
-      };
-
-      res.redirect(`${FRONTEND_URI}?${querystring.stringify(tokens)}`);
+      }).toString();
+      res.redirect(`${FRONTEND_URI}#${params}`);
+    } else {
+      res.send({ error })
     }
-  });
+  };
+
+  request.post(authOptions, callback);
 });
 
-app.listen(PORT, function () {
-  console.log(`Node server listening on port ${PORT}`);
+////// REFRESH TOKEN ROUTE
+app.get('/refresh_token', (req, res) => {
+  const refresh_token = req.query.refresh_token;
+  const buffer = new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+    'base64'
+  );
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    headers: {
+      Authorization: `Basic ${buffer}`,
+    },
+    json: true,
+  };
+
+  const callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const access_token = body.access_token;
+      res.send({ access_token })
+    }
+  };
+
+  request.post(authOptions, callback);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`);
 });

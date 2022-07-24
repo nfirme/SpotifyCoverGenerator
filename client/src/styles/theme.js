0ff34e21e7d699f3cc6495046e60d7548dd391be
Nick Firme <nickfirme@live.com>
2022-07-24 16:39:47 -0700

    added Inter font

--- client/src/styles/theme.js
+++ client/src/styles/theme.js
@@ -4,15 +4,18 @@ const theme = {
     black: '#181818',
     sky: '#38bdf8',
     purple: '#c084fc',
-    orange: '#fb923c',
+    pink: '#f472b6',
     red: '#f87171',
     green: '#10b981',
     darkGreen: '#047857',
     lightGreen: '#6ee7b7',
+    gray: '#27272a',
+    darkGray: '#202021',
+    lightGray: '#3f3f46',
   },
   fonts: {
     primary:
-      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+      'Inter, system, -apple-system, BlinkMacSystemFont, sans-serif',
   },
   fontSizes: {
     base: `16px`,
